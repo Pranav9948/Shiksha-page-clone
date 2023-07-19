d37f0b6c8df697b9c87283aa462ed324a112d17a
Pranav P <pranavat9948@gmail.com>
2023-07-20 00:25:53 +0530

    NAVBAR COMPLETED , yet to complete footer

--- src/components/Navbar/DekstopNav.js
+++ src/components/Navbar/DekstopNav.js
@@ -5,54 +5,73 @@ import { MdArrowDropDown } from "react-icons/md";
 const DekstopNav = () => {
   return (
     <nav className="bg-navBackgrndColor p-4">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className=" mx-auto flex justify-between items-center">
         {/* Your Logo */}
         <div className="flex items-center space-x-4">
-          {/* <img src="/images/logos.png" className=' pt-3 ps-3 w-10' /> */}
-          
-          {/* <h4 className="font-bold text-3xl font-arial text-white">shiksha</h4> */}
-          </div>
+          <img src="/images/logos.png" className=' pt-3 ps-3 w-10' />
+           <h4 className="font-bold text-3xl font-arial text-white">shiksha</h4>
+        </div>
         
-
         {/* Search Bar */}
-        <div className='flex items-center pe-32'>
-        <div className="flex items-center justify-center w-full">
-          <input
-            type="text"
-            className="rounded-l-lg py-3 px-60 max-w-full bg-white focus:outline-none"
-            placeholder="Search colleges,courses,exams,QnA,& Articles"
-          />
-          <button className="bg-orange-500 py-3 px-10 rounded-r-lg text-white">Search</button>
+        <div className='flex items-center ms-20'>
+          <div className="flex items-center  space-x-2">
+            <input
+              type="text"
+              className="rounded-l-lg py-2 px-4 max-w-full bg-white focus:outline-none  w-[500px]" 
+              placeholder="Search colleges, courses, exams, QnA, & Articles"
+            />
+            <button className="bg-orange-500 py-2 px-10 rounded-r-lg text-white" style={{marginLeft:'-30px'}}>Search</button>
+          </div>
         </div>
 
         {/* Right-Side Query */}
-        <div className="text-white flex items-center "><AiOutlineWhatsApp/>Ask a query on<span className='font-bold ps-5'>8826978461</span> </div>
+        <div className="text-white flex items-center ps-5 ms-20">
+          <AiOutlineWhatsApp className="mr-2 ps-10" />
+          Ask a query on <span className='font-bold ps-3'>8826978461</span>
         </div>
       </div>
 
       {/* Navigation Links */}
-      <div className="container mx-auto mt-4 flex justify-center space-x-6">
-        <a href="#" className="text-white font-bold text-sm ps-2 flex items-center">MBA  <span className='ps-1 font-bold text-xl'><MdArrowDropDown/></span></a>
-        <a href="#" className="text-white font-bold text-sm ps-2 flex items-center">ENGINEERING <span className='ps-1 font-bold text-xl'><MdArrowDropDown/></span></a>
-        <a href="#" className="text-white font-bold text-sm ps-2 flex items-center">SCIENCE <span className='ps-1 font-bold text-xl'><MdArrowDropDown/></span></a>
-        <a href="#" className="text-white font-bold text-sm ps-2 flex items-center">ARTS <span className='ps-1 font-bold text-xl'><MdArrowDropDown/></span></a>
-        <a href="#" className="text-white font-bold text-sm ps-2 flex items-center">LAW <span className='ps-1 font-bold text-xl'><MdArrowDropDown/></span></a>
-        <a href="#" className="text-white font-bold text-sm ps-2 flex items-center">MEDICAL <span className='ps-1 font-bold text-xl'><MdArrowDropDown/></span></a>
-        
-        <div className='flex items-end '>
-        <a href="#" className="text-white font-bold text-sm ps-5">Login  </a>
-        <a href="#" className="text-white font-bold text-sm ps-5">Signup </a>
+      <div className="container mx-auto ms-56 mt-4 flex justify-between items-center space-x-0">
+ <div>
+  <a href="#" className="text-white font-bold text-sm flex items-center ms-10">
+    MBA <span className='pl-1 text-xl'><MdArrowDropDown /></span>
+  </a>
+  </div>
+  <div>
+  <a href="#" className="text-white font-bold text-sm flex items-center -ms-28">
+    ENGINEERING <span className='pl-1 text-xl'><MdArrowDropDown /></span>
+  </a>
+
+  </div>
+  <div>
+  <a href="#" className="text-white font-bold text-sm flex items-center -ms-24">
+    SCIENCE <span className='pl-1 text-xl'><MdArrowDropDown /></span>
+  </a>
+  </div>
+  <div>
+  <a href="#" className="text-white font-bold text-sm flex items-center -ms-24">
+    ARTS <span className='pl-1 text-xl'><MdArrowDropDown /></span>
+  </a>
+  </div>
+  <div>
+  <a href="#" className="text-white font-bold text-sm flex items-center -ms-32">
+    LAW <span className='pl-1 text-xl'><MdArrowDropDown /></span>
+  </a>
+  </div>
+  <div>
+  <a href="#" className="text-white font-bold text-sm flex items-center -ms-44">
+    BUISNESS <span className='pl-1 text-xl'><MdArrowDropDown /></span>
+  </a>
+  </div>
+ 
+
+
+        <div className='flex items-end ps-5'>
+          <a href="#" className="text-white font-bold text-sm ps-10">Login</a>
+          <a href="#" className="text-white font-bold text-sm ps-5">Signup</a>
         </div>
-      </div> 
-
-
-    
-     
-
-     
-
-
-
+      </div>
     </nav>
   );
 };
